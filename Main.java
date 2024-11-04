ba4150d77367e6626c052056dac1f85fda613726
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-11-04 14:19:50 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,46 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        WhileLoopFun fun = new WhileLoopFun();
-
-        System.out.println("-- test printDigits --");
-        fun.printDigits(5);
-        System.out.println("---");
-        fun.printDigits(361);
-        System.out.println("---");
-        fun.printDigits(98000);
-        System.out.println("---");
-        fun.printDigits(150856);
-        
-        System.out.println("-- test countLetter --");
-        System.out.println(fun.countLetter("apple", "a"));
-        System.out.println(fun.countLetter("apple", "z"));
-        System.out.println(fun.countLetter("babababa", "a"));
-        System.out.println(fun.countLetter("babababa", "b"));
-        System.out.println(fun.countLetter("excellente", "e"));
-        System.out.println(fun.countLetter("rawrrrrrr", "r"));
-        
-        System.out.println("-- test maxDoubles --");
-        System.out.println(fun.maxDoubles(4, 20));
-        System.out.println(fun.maxDoubles(2, 65));
-        System.out.println(fun.maxDoubles(5, 500));
-        System.out.println(fun.maxDoubles(2, 64));
-        System.out.println(fun.maxDoubles(2, 63));
-        System.out.println(fun.maxDoubles(8, 10));
-        System.out.println(fun.maxDoubles(8, 5));
-        System.out.println(fun.maxDoubles(10, 456000));
-        
-        System.out.println("-- test isPrime --");
-        System.out.println(fun.isPrime(2));
-        System.out.println(fun.isPrime(13));
-        System.out.println(fun.isPrime(773));
-        System.out.println(fun.isPrime(9857));
-        System.out.println(fun.isPrime(101879));
-        System.out.println(fun.isPrime(1));
-        System.out.println(fun.isPrime(14));
-        System.out.println(fun.isPrime(30));
-        System.out.println(fun.isPrime(771));
-        System.out.println(fun.isPrime(93243));
-        
+        StringLoops loops = new StringLoops();
+        System.out.println("--- testing countCharacters ---");
+        System.out.println(loops.countCharacters("a", "Apple and banana"));
+        System.out.println(loops.countCharacters("A", "Apple and banana"));
+        System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
+        System.out.println(loops.countCharacters("g", "Hello! Nice day!"));
+        System.out.println("\n--- testing reverseString ---");
+        System.out.println(loops.reverseString("hello!"));
+        System.out.println(loops.reverseString("Apples and bananas"));
+        System.out.println(loops.reverseString("C"));
     }
 }
     
